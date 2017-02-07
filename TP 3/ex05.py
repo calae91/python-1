ddadb87dde56eaed90de533ccb052c7eb7eca931
user <briand78410@gmail.com>
2017-02-07 18:17:28 +0100

    inverted repeat

--- TP 3/ex05.py
+++ TP 3/ex05.py
@@ -5,10 +5,20 @@ ch = raw_input(colors.warning("saisir chaine : "))
 ch = ch.upper()
 ch = ch.replace(" ", "")
 i = 0
-while i < len(ch)/2 and ch[i] == ch[len(ch)-i-1]:
+err = 0
+while i < len(ch)/2 and err < 1:
+	if ch[i] == 'T' and ch[len(ch) -i-1] != 'A':
+		err += 1
+	elif ch[i] == 'A' and ch[len(ch) -i-1] != 'T' :
+		err +=1
+	elif ch[i] == 'G' and ch[len(ch) -i-1] != 'C' :
+		err +=1
+	elif ch[i] == 'C' and ch[len(ch) -i-1] != 'G' :
+		err +=1
+	else :
 		i+=1
 		
-if i == len(ch)/2:
-	print colors.success ("votre sequence est un palindrome")
+if err == 0 :
+	print colors.success ("INVERTED REPEAT")
 else :
-	print colors.error("pas un palindrome")
+	print colors.error("NOT INVERTED REPEAT")
